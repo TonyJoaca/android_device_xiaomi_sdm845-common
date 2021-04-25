bdc8d32f8e8399343d43970c7778001c0b3e78f7
TonyJoaca <dj_tony2013@yahoo.ro>
2021-04-25 19:42:35 +0000

    Revert "Revert "sdm845-common: Use component-override to configure Bluetooth""
    This reverts commit 037d9588475746e171caf4b13881efc20cba396e.

--- sdm845.mk
+++ sdm845.mk
@@ -14,6 +14,11 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vend
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
