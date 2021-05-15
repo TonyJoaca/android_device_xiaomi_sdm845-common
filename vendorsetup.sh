5ee3b318250b53372746be2006a55684400474d7
TonyJoaca <dj_tony2013@yahoo.ro>
2021-05-15 10:31:11 +0000

    sdm845-common: Clone hals from los

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,11 @@
 export SKIP_ABI_CHECKS=true
+
+# delete
+rm -rf hardware/qcom-caf/sdm845/display
+rm -rf hardware/qcom-caf/sdm845/audio
+rm -rf hardware/qcom-caf/sdm845/media
+
+# clone
+git clone -b lineage-18.1-caf-sdm845 https://github.com/LineageOS/android_hardware_qcom_display hardware/qcom-caf/sdm845/display
+git clone -b lineage-18.1-caf-sdm845 https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom-caf/sdm845/audio
+git clone -b lineage-18.1-caf-sdm845 https://github.com/LineageOS/android_hardware_qcom_media hardware/qcom-caf/sdm845/media
