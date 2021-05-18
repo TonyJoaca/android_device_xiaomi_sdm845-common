93ebbade57c07c6405434b3d9779dbc103a934bc
TonyJoaca <dj_tony2013@yahoo.ro>
2021-05-18 15:59:09 +0000

    sdm845-common: Remove lineage.dependencies and update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,8 @@ rm -rf hardware/qcom-caf/sdm845/media
 git clone -b lineage-18.1-caf-sdm845 https://github.com/LineageOS/android_hardware_qcom_display hardware/qcom-caf/sdm845/display
 git clone -b lineage-18.1-caf-sdm845 https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom-caf/sdm845/audio
 git clone -b lineage-18.1-caf-sdm845 https://github.com/LineageOS/android_hardware_qcom_media hardware/qcom-caf/sdm845/media
+git clone -b lineage-18.1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+
+cd hardware/qcom-caf/sdm845/display
+git revert 3a9e02ba03bbc0466d1cf1087e071c46d57e24ba
+cd -
